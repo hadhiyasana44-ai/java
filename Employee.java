8ad65e2e1e0bddbe7b42173a932beadea3671c86
Hadhiya <hadhiyasana44@gmail.com>
2026-08-21 21:01:01 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,10 @@
+package tutorial6;
+
+public class Employee {
+
+    protected double salary = 50000;
+
+    protected void showSalary() {
+        System.out.println("Salary: " + salary);
+    }
+}
